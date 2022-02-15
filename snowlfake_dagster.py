5a385e940c4afd6381b0e25262abf1da565de877
condosr <scondo@bearcognition.com>
2022-02-15 09:42:01 -0500

    working bare minimum snowflake integration

--- snowlfake_dagster.py
+++ snowlfake_dagster.py
@@ -1,12 +1,24 @@
 from dagster import job, op
 from dagster_snowflake import snowflake_resource
+import os
 
+
+os.environ["SNOWFLAKE_ACCOUNT"] = "maa23857-main"
+os.environ["SNOWFLAKE_USER"] = "mindsdb"
+os.environ["SNOWFLAKE_PASSWORD"] = "o7De.zGlTc]sI7v4py2X"
+os.environ["SNOWFLAKE_DATABASE"] = "FIVETRAN_DATABASE"
+os.environ["SNOWFLAKE_SCHEMA"] = "DUCDUC_SHOPIFY"
+os.environ["SNOWFLAKE_WAREHOUSE"] = "FIVETRAN_WAREHOUSE"
+print('________________________________')
+print(os.environ["SNOWFLAKE_USER"])
+print('________________________________')
 @op(required_resource_keys={'snowflake'})
 def get_one(context):
-    context.resources.snowflake.execute_query('SELECT 1')
+    context.resources.snowflake.execute_query('SELECT * from "ORDER"')
 
 @job(resource_defs={'snowflake': snowflake_resource})
 def my_snowflake_job():
+    
     get_one()
 
 my_snowflake_job.execute_in_process(
@@ -25,3 +37,15 @@ my_snowflake_job.execute_in_process(
         }
     }
 )
+
+
+
+'''
+SNOWFLAKE_HOST = 'maa23857-main.snowflakecomputing.com'
+SNOWFLAKE_USER = 'mindsdb'
+SNOWFLAKE_PASS = 'o7De.zGlTc]sI7v4py2X'
+SNOWFLAKE_DB = 'FIVETRAN_DATABASE'
+SNOWFLAKE_ACCOUNT = 'maa23857-main'
+SNOWFLAKE_SCHEMA = 'DUCDUC_SHOPIFY'
+SNOWFLAKE_WAREHOUSE = 'FIVETRAN_WAREHOUSE'
+'''
